72b71bb3b4ba0d94f60cabd1305a0e705aaa12b3
SanthoshChandru07 <santhosh777chandru@gmail.com>
2025-04-27 13:36:34 +0530

    day1

---
+++ day1/Datatypes.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Datatypes {
+	public static void main(String[] args) {
+		byte age=27;
+		System.out.println(age);
+		int seriesofnumbers=123456789;
+		System.out.println(seriesofnumbers);
+		long phno=8344225532L;
+		System.out.println(phno);
+		short year=1997;
+		System.out.println(year);
+		float speed=1.1298765432123f;
+		System.out.println(speed);
+		double speed1= 1.1298765432123;
+		System.out.println(speed1);
+		char alphabet='S';
+		System.out.println(alphabet);
+		boolean isVisible=true;
+		System.out.println(isVisible);
+		String weekend="Sunday";
+		System.out.println(weekend);
+	}
+
+}
+
